a881493fb3e376bb6c207f85924e55afdb12d8fd
Sergei <hentai.exe@gmail.com>
2023-02-19 00:44:17 +0300

    init

---
+++ src/qback/core.clj
@@ -0,0 +1,10 @@
+(ns qback.core
+  (:gen-class)
+  (:require [qback.controller.handlers :as controller]
+            [ring.adapter.jetty :as jetty]))
+
+(defn -main 
+  [& args]
+  (jetty/run-jetty controller/what-is-my-ip {:port 3000
+                                             :join? false}))
+
