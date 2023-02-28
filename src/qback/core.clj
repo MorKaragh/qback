c290046464dbc2b1d2fd73efde8fc1920c29c06e
Sergei <hentai.exe@gmail.com>
2023-02-28 23:25:23 +0300

    reitit

--- src/qback/core.clj
+++ src/qback/core.clj
@@ -1,10 +1,8 @@
 (ns qback.core
   (:gen-class)
-  (:require [qback.controller.handlers :as controller]
+  (:require [qback.controller.main-controller :as controller]
             [ring.adapter.jetty :as jetty]))
 
 (defn -main 
-  [& args]
-  (jetty/run-jetty controller/my-ip {:port 3000
-                                             :join? false}))
+  [& args])
 
